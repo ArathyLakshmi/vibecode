e695bbe28d22be4112c625d19686335348902abb
ArathyLakshmi <arathy.L@gmail.com>
2026-02-10 12:02:11 -0500

    ci: add GitHub Actions workflow for tests and client build

---
+++ my-project-today/src/tests/MeetingRequests.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace MeetingRequests.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MeetingRequestsDbContext>));
+            if (descriptor != null) services.Remove(descriptor);
+
+            services.AddDbContext<MeetingRequestsDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<MeetingRequestsDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
